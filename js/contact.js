8bcf54fe6df4ea55b71547d8cd0a53b6a8b6c2fc
Neil Sukalikar <neil.sukalikar@gmail.com>
2022-04-12 20:56:18 +0530

    initial version

---
+++ js/contact.js
@@ -0,0 +1,189 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Google Map
+
+
+******************************/
+
+$(document).ready(function () {
+
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var header = $('.header');
+	var menu = $('.menu');
+	var burger = $('.hamburger');
+	var menuActive = false;
+	var map;
+
+	setHeader();
+
+	$(window).on('resize', function () {
+		setHeader();
+
+		if (window.innerWidth > 1280) {
+			if (menuActive) {
+				closeMenu();
+			}
+		}
+		setTimeout(function () {
+			jQuery(".main_content_scroll").mCustomScrollbar("update");
+		}, 375);
+	});
+
+	$(document).on('scroll', function () {
+		setHeader();
+	});
+
+	initMenu();
+	initGoogleMap();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader() {
+		if ($(window).scrollTop() > 91) {
+			header.addClass('scrolled');
+		}
+		else {
+			header.removeClass('scrolled');
+		}
+	}
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+	function initMenu() {
+		if ($('.hamburger').length && $('.menu').length) {
+			var hamb = $('.hamburger');
+
+			hamb.on('click', function () {
+				if (!menuActive) {
+					openMenu();
+				}
+				else {
+					closeMenu();
+				}
+			});
+		}
+	}
+
+	function openMenu() {
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu() {
+		menu.removeClass('active');
+		menuActive = false;
+	}
+
+	/* 
+
+	4. Init Google Map
+
+	*/
+
+	function initGoogleMap() {
+		var myLatlng = new google.maps.LatLng(18.9720,73.0348);
+		var mapOptions =
+		{
+			center: myLatlng,
+			zoom: 14,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			draggable: true,
+			scrollwheel: true,
+			zoomControl: true,
+			zoomControlOptions:
+			{
+				position: google.maps.ControlPosition.RIGHT_CENTER
+			},
+			mapTypeControl: false,
+			scaleControl: false,
+			streetViewControl: false,
+			rotateControl: false,
+			fullscreenControl: true,
+			styles:
+				[
+					{
+						"featureType": "road.highway",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#ffeba1"
+							}
+						]
+					}
+				]
+		}
+
+		// Initialize a map with options
+		map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+		// Re-center map after window resize
+		google.maps.event.addDomListener(window, 'resize', function () {
+			setTimeout(function () {
+				google.maps.event.trigger(map, "resize");
+				map.setCenter(myLatlng);
+			}, 1400);
+		});
+	}
+
+	/*
+	5. Send Messages
+	*/
+
+	$("#contact_form").validator().on("submit", function (event) {
+		if (event.isDefaultPrevented()) {
+			// handle the invalid form...
+			alert('Kindly update all the details')
+		} else {
+			// everything looks good!
+			event.preventDefault();
+			submitForm();
+		}
+	});
+
+	function submitForm() {
+		var name = $("#txtName").val();
+		var email = $("#txtEmail").val();
+		var subject = $("#txtSubject").val();
+		var message = $("#txtMessage").val();
+		var contactForm = {
+			name,
+			email,
+			subject,
+			message
+		};
+		$.ajax({
+			type: "POST",
+			url: "http://service.neilsukalikar.info/api/home",
+			data: contactForm,
+			success: function (text) {
+				if (text == "success") {
+					alert('Message sent successfully')
+				} else {
+					alert("Something went wrong");
+				}
+			}
+		});
+	}
+});
